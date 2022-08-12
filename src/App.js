5f70c644abd32bf77ca04bfefe797b2c2a3fe6eb
jeannie <jeanniezheng123@gmail.com>
2022-08-12 11:45:48 -0500

    Added classNames for styling

--- src/App.js
+++ src/App.js
@@ -10,7 +10,8 @@ import Setup from './Pages/Setup';
 import "./App.css";
 import Profile from './Pages/Profile';
 import { Lesson1 } from './Pages/Lesson1';
-
+import LessonQuiz1 from './Pages/SubPages/LessonQuiz1';
+import { Lesson2 } from './Pages/Lesson2';
 
 
 // TODO: support different amount of notes than 4
@@ -21,13 +22,20 @@ function App() {
     <>
       <Router>
         <NavDisplay />
+        {/* <div>
 
-        <a href="/"> Go to Home Page</a>
+
+          <a className="home-page-button" href="/"> Go to Home Page</a>
+        </div> */}
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/lesson1" element={<Lesson1 />} />
           <Route path="/lessonDisplay" element={<LessonDisplay />} />
+          <Route path="/lessonquiz1" element={<LessonQuiz1 />} />
           <Route path="/setup" element={<Setup />} />
+          <Route path="/lesson2" element={<Lesson2 />} />
+
+
 
         </Routes>
 
