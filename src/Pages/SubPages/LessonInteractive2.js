85653aff4d71c74a7c67ffae021269cbd8a3ea26
jeannie <jeanniezheng123@gmail.com>
2023-02-20 12:29:45 -0600

    refactor

--- src/Pages/SubPages/LessonInteractive2.js
+++ src/Pages/SubPages/LessonInteractive2.js
@@ -1,23 +1,18 @@
-import React from 'react'
+import { React, useReducer } from 'react'
 import Music from "../../Music";
 import { useMIDIEvent, MIDI_KEYDOWN, MIDI_KEYUP } from "../../midi";
-import { useReducer } from "react";
 
 
 const ACTION_KEYDOWN = "ACTION_KEYDOWN";
 
-
+// If action.type === action_keydown and action.key === expectedNote, then expectedNote should be red. Else, the expectedNote should be black and new expectedNote should be rendered
 const reducer = (state, action) => {
     switch (action.type) {
-        //if action.type is action_keydown, then expectedNote = current toPlay[position]
         case ACTION_KEYDOWN:
             console.log("action dispatched:", action);
-
             const expectedNote = state.toPlay[0];
-            console.log("expected note:", expectedNote);
             if (expectedNote !== action.key) {
                 console.log("Wrong note!");
-
                 return {
                     ...state,
                     fontColor: "Red"
@@ -29,31 +24,24 @@ const reducer = (state, action) => {
                     ...state,
                     fontColor: "Black",
                     toPlay: generateRandom()
-                    // toPlay: setTimeout(generateRandom, 3000)
                 }
-
             }
-
         default:
             throw new Error(`Unknown action type: ${action.type}`);
     }
 }
 
-
+//Generate random random piano notes(expectedNote) from octive 4 to 5
 const generateRandom = () => {
     const notes = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
     const num = ["4", "5"]
-
     const new_note = notes[Math.floor(Math.random() * notes.length)] + num[Math.floor(Math.random() * num.length)];
-
-
-
     return new_note
 
 }
 
+// Renders the note needed to be played, listens to the MIDIEvent and rerenders depending on note played
 const LessonInteractive2 = () => {
-
     const [state, dispatch] = useReducer(reducer, {
         toPlay: 'C4',
         fontColor: 'Black'
@@ -68,23 +56,15 @@ const LessonInteractive2 = () => {
         }
     });
 
-    //have a class name called red. if bahbahbah then red else green
-
     return (
         <section className="parent">
-
             <div className='lesson-reading-container section' >
                 <h1 className="reading-title">Please Connect Piano</h1>
                 <h3>On your keyboard, press the note:</h3>
                 <h2 className={`${state.fontColor} piano-note`}> {state.toPlay.slice()}</h2>
                 {state.fontColor === "Red" && <h1>Wrong Note, try again</h1>}
-                {/* <Music className="staff"
-                notes={notesToEasyScore(state.toPlay)}
-            /> */}
             </div>
         </section>
-
     )
 }
-
 export default LessonInteractive2
