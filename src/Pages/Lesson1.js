f400204391b5ab42b3327885b9f9925cd5ee810f
jeannie <jeanniezheng123@gmail.com>
2023-04-10 11:38:23 -0500

    Refactor lesson 1

--- src/Pages/Lesson1.js
+++ src/Pages/Lesson1.js
@@ -1,44 +1,40 @@
-import SplitPane from "react-split-pane"
-import "../stylesheet/editor1.css"
-import Home from "./Home"
-import MusicGenerate from "../musicGenerate"
-import LessonReading1 from "./SubPages/LessonReading1"
-import LessonInteractive1 from "./SubPages/LessonInteractive1"
-import LessonQuiz1 from "./SubPages/LessonQuiz1"
-import { useState } from "react"
-import { useNavigate } from "react-router-dom"
-import Button from 'react-bootstrap/Button'
-import Modal from 'react-bootstrap/Modal'
-import '../stylesheet/lesson-reading.css'
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import SplitPane from "react-split-pane";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import Home from "./Home";
+import MusicGenerate from "../musicGenerate";
+import LessonReading1 from "./SubPages/LessonReading1";
+import LessonInteractive1 from "./SubPages/LessonInteractive1";
+import LessonQuiz1 from "./SubPages/LessonQuiz1";
+import "../stylesheet/lesson-reading.css";
+import "../stylesheet/editor1.css";
 
-
-export const Lesson1 = () => {
-    let navigate = useNavigate()
+const Lesson1 = () => {
+    const navigate = useNavigate();
     const [isShown, setIsShown] = useState(false);
 
-
-    const handleClick = event => {
-        setIsShown(current => !current);
+    const handleClick = (event) => {
+        setIsShown((current) => !current);
     };
-
-
 
     return (
         <SplitPane
             className="scroll"
             split="vertical"
             minSize={50}
-            defaultSize={parseInt(localStorage.getItem('splitPos'), 10)}
-            onChange={(size) => localStorage.setItem('splitPos', size)}>
+            defaultSize={parseInt(localStorage.getItem("splitPos"), 10)}
+            onChange={(size) => localStorage.setItem("splitPos", size)}
+        >
             <div>
                 <LessonReading1 />
-
             </div>
             <div>
                 <LessonInteractive1 />
-
             </div>
         </SplitPane>
-        // </SplitPane>
-    )
-}
+    );
+};
+
+export default Lesson1;
