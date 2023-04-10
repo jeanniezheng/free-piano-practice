67287b441796e857b5b96b7be559b1aca834a4c7
jeannie <jeanniezheng123@gmail.com>
2023-04-10 11:33:37 -0500

    Refactor Lesson Display

--- src/components/LessonDisplay.js
+++ src/components/LessonDisplay.js
@@ -1,56 +1,32 @@
-import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPlay, faMusic, faFileAudio, faGear } from '@fortawesome/free-solid-svg-icons'
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlay, faMusic, faFileAudio, faGear } from '@fortawesome/free-solid-svg-icons';
+import { useNavigate } from 'react-router-dom';
 
-import MusicGenerate from '../musicGenerate'
-import "../stylesheet/lessons.css"
-import Music from '../Music'
-import { useNavigate } from 'react-router-dom'
+import '../stylesheet/lessons.css';
 
+const LessonDisplay = () => {
+    const navigate = useNavigate();
 
-function LessonDisplay() {
-    let navigate = useNavigate();
+    const buttons = [
+        { title: 'Setup', icon: faGear, route: '/setup' },
+        { title: 'Lesson 1', icon: faPlay, route: '/lesson1' },
+        { title: 'Lesson 2', icon: faMusic, route: '/lesson2' },
+        { title: 'Lesson 3', icon: faFileAudio, route: '/lesson3' },
+        { title: 'Sheet Music', icon: null, route: '/sheetmusic' },
+    ];
 
     return (
-        <>
-            <section className='center'>
-                <div>
-                    <button className='button' onClick={() => {
-                        navigate("/setup")
-                    }}> <FontAwesomeIcon className="icon" icon={faGear} size="lg" /> Setup
+        <section className='center'>
+            {buttons.map((button) => (
+                <div key={button.route} className='button-div'>
+                    <button className='button' onClick={() => navigate(button.route)}>
+                        {button.icon && <FontAwesomeIcon className='icon' icon={button.icon} size='lg' />} {button.title}
                     </button>
                 </div>
+            ))}
+        </section>
+    );
+};
 
-                <div className="button-div">
-                    <button className='button' onClick={() => {
-                        navigate("/lesson1")
-                    }}> <FontAwesomeIcon className="icon" icon={faPlay} size="lg" /> Lesson 1
-                    </button>
-                </div>
-
-                <div>
-                    <button className='button' onClick={() => {
-                        navigate("/lesson2")
-                    }}><FontAwesomeIcon className="icon" icon={faMusic} size="lg" /> Lesson 2</button>
-                </div>
-
-                <div>
-                    <button className='button' onClick={() => {
-                        navigate("/lesson3")
-                    }}> <FontAwesomeIcon className="icon" icon={faFileAudio} size="lg" />Lesson 3</button>
-                </div>
-                <div>
-
-                    <button onClick={() => {
-                        navigate("/sheetmusic")
-                    }} className='button'>Sheet Music</button>
-                </div>
-            </section >
-
-
-        </>
-
-    )
-}
-
-export default LessonDisplay
+export default LessonDisplay;
