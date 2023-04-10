dbbe4f3ec8ae9f6b5da3f53148ffae8cd7c5fefe
jeannie <jeanniezheng123@gmail.com>
2023-04-10 11:37:06 -0500

    Refactor lesson 2

--- src/Pages/Lesson2.js
+++ src/Pages/Lesson2.js
@@ -1,42 +1,34 @@
-import SplitPane from "react-split-pane"
-import "../stylesheet/editor1.css"
-import Home from "./Home"
-import MusicGenerate from "../musicGenerate"
-import LessonReading1 from "./SubPages/LessonReading1"
-import LessonInteractive2 from "./SubPages/LessonInteractive2"
-import LessonQuiz1 from "./SubPages/LessonQuiz1"
-import { useState } from "react"
-import { useNavigate } from "react-router-dom"
-import Button from 'react-bootstrap/Button'
-import Modal from 'react-bootstrap/Modal'
-import LessonReading2 from "./SubPages/LessonReading2"
+import SplitPane from "react-split-pane";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import LessonReading2 from "./SubPages/LessonReading2";
+import LessonInteractive2 from "./SubPages/LessonInteractive2";
+
+import "../stylesheet/editor1.css";
 
 export const Lesson2 = () => {
-    let navigate = useNavigate()
+    const navigate = useNavigate();
     const [isShown, setIsShown] = useState(false);
-
 
     const handleClick = event => {
         setIsShown(current => !current);
     };
 
-
-
     return (
-
-        <SplitPane split="vertical"
+        <SplitPane
+            split="vertical"
             minSize={50}
             defaultSize={parseInt(localStorage.getItem('splitPos'), 10)}
-            onChange={(size) => localStorage.setItem('splitPos', size)}>
+            onChange={(size) => localStorage.setItem('splitPos', size)}
+        >
             <div>
                 <LessonReading2 />
-                {/* <MusicGenerate /> */}
-
             </div>
             <div>
                 <LessonInteractive2 />
             </div>
         </SplitPane>
-        // </SplitPane>
-    )
-}
+    );
+};
