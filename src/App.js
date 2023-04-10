105542e7cc702b41f41b2c3124e1ab9996a51c5e
jeannie <jeanniezheng123@gmail.com>
2023-04-09 22:32:05 -0500

    Refactor App.js

--- src/App.js
+++ src/App.js
@@ -1,36 +1,27 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import Setup from './Pages/Setup';
-import MusicGenerate from "./musicGenerate";
-import Home from "./Pages/Home"
-import About from "./components/LessonDisplay";
-import LessonDisplay from "./components/LessonDisplay";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NavDisplay from "./components/Nav";
-// import GenerateQuote from "./components/GenerateQuote";
+import LessonDisplay from "./components/LessonDisplay";
+import About from "./components/LessonDisplay";
+import MusicGenerate from "./musicGenerate";
 import Profile from './Pages/Profile';
+import Setup from './Pages/Setup';
+import SheetMusic from './Pages/SheetMusic';
 import LessonQuiz1 from './Pages/SubPages/LessonQuiz1';
+import Home from "./Pages/Home";
 import { Lesson1 } from './Pages/Lesson1';
 import { Lesson2 } from './Pages/Lesson2';
 import { Lesson3 } from './Pages/Lesson3';
-import SheetMusic from './Pages/SheetMusic';
 import FurElise from './SheetMusic/FurElise';
 import FlyMeToTheMoon from './SheetMusic/FlyMeToTheMoon';
-
-
-// TODO: support different amount of notes than 4
+import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-
   return (
     <>
       <Router>
         <NavDisplay />
-        {/* <div>
 
-
-          <a className="home-page-button" href="/"> Go to Home Page</a>
-        </div> */}
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/lesson1" element={<Lesson1 />} />
@@ -40,17 +31,12 @@ function App() {
           <Route path="/lesson2" element={<Lesson2 />} />
           <Route path="/lesson3" element={<Lesson3 />} />
 
-          <Route path="/sheetmusic" element={< SheetMusic />} />
+          <Route path="/sheetmusic" element={<SheetMusic />} />
           <Route path="/sheetmusic/furelise" element={<FurElise />} />
           <Route path="/sheetmusic/flymetothemoon" element={<FlyMeToTheMoon />} />
-
-
-
         </Routes>
-
       </Router>
     </>
-
   );
 }
 
